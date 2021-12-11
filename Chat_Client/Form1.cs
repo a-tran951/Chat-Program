6eb7f53c50ab42cfcfd7056ed164e41d190e245e
a-tran951 <a.tran951@mybvc.ca>
2021-12-10 18:28:32 -0700

    Add project files.

---
+++ Chat_Client/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace Chat_Client
+{
+    public partial class Form1 : Form
+    {
+        TcpClient tcpClient;
+        string clientUserName;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            StreamWriter sWriter = new StreamWriter(tcpClient.GetStream());
+            sWriter.WriteLine("EXITCLIENTCLOSE_" + clientUserName);
+        }
+        static void SendDataToServer(object obj)
+        {
+            try
+            {
+                TcpClient tcpClient = (TcpClient)obj;
+                StreamWriter sWriter = new StreamWriter(tcpClient.GetStream());
+
+                while (true)
+                {
+                    if (tcpClient.Connected)
+                    {
+                        string input = Console.ReadLine();
+                        sWriter.WriteLine(input);
+                        sWriter.Flush();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+            }
+        }
+        static void ReadDataFromServer(object obj)
+        {
+            TcpClient tcpClient = (TcpClient)obj;
+            StreamReader sReader = new StreamReader(tcpClient.GetStream());
+
+            while (true)
+            {
+                try
+                {
+                    string message = sReader.ReadLine();
+                    Console.WriteLine(message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    break;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs eventArgument)
+        {
+            try
+            {
+                tcpClient = new TcpClient("127.0.0.1", 4004);
+                StreamWriter sWriter = new StreamWriter(tcpClient.GetStream());
+                Console.WriteLine("Connected to server.");
+                Console.WriteLine("Please enter a username");
+                clientUserName = Regex.Replace(Console.ReadLine(), @"\s", "");
+                sWriter.WriteLine(clientUserName);
+                Thread thread = new Thread(ReadDataFromServer);
+                thread.Start(tcpClient);
+
+                Thread thread2 = new Thread(SendDataToServer);
+                thread2.Start(tcpClient);
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Chat_Server/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace Server
+{
+    class Program
+    {
+        private static TcpListener server;
+        private static List<TcpClient> tcpClientsList = new List<TcpClient>();
+
+        static void Main(string[] args)
+        {
+            ServerListener();
+        }
+
+        public static void ServerListener()
+        {
+            server = new TcpListener(IPAddress.Any, 4004);
+            server.Start();
+
+            Console.WriteLine("Server started");
+
+            while (true)
+            {
+                TcpClient tcpClient = server.AcceptTcpClient();//each time connection established it will be stored in a list
+                tcpClientsList.Add(tcpClient);
+
+                Thread thread = new Thread(ClientListener);//each client connection need to run in parallel 
+                thread.Start(tcpClient);
+
+            }
+        }
+
+        public static void ClientListener(object obj)
+        {
+            TcpClient tcpClient = (TcpClient)obj;
+            StreamReader reader = new StreamReader(tcpClient.GetStream());
+            Regex regex = new Regex(@"^EXITCLIENTCLOSE_[a-zA-Z0-9]{1,}");
+            string message = reader.ReadLine();
+            Console.WriteLine("Client is connected");
+            bool isDone = false;
+            foreach (TcpClient client in tcpClientsList)
+            {
+                StreamWriter sWriter = new StreamWriter(client.GetStream());
+                sWriter.WriteLine(message + " has joined the chat");
+                sWriter.Flush();
+            }
+            while (!isDone)
+            {
+                message = reader.ReadLine();
+                if (regex.IsMatch(message))
+                {
+                    string userName = message.Split("_")[1];
+                    tcpClientsList.Remove(tcpClient);
+                    foreach (TcpClient client in tcpClientsList)
+                    {
+                        StreamWriter sWriter = new StreamWriter(client.GetStream());
+                        sWriter.WriteLine(userName + " has left the chat");
+                        sWriter.Flush();
+                    }
+                    isDone = true;
+                }
+                //Broadcast message to all client
+                foreach (TcpClient client in tcpClientsList)
+                {
+                    StreamWriter sWriter = new StreamWriter(client.GetStream());
+                    sWriter.WriteLine(message);
+                    sWriter.Flush();
+
+                }
+                Console.WriteLine(message);
+            }
+        }
+
+
+    }
+}
